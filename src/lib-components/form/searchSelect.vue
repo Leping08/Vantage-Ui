c7d4eda3e80aa4da0ca57ae66f2f3395a38c4cba
Leping08 <dereks008@gmail.com>
2021-11-24 08:20:20 -0500

    Turn off autocomplete on search select

--- src/lib-components/form/searchSelect.vue
+++ src/lib-components/form/searchSelect.vue
@@ -24,6 +24,7 @@
           v-model="input"
           :id="label"
           type="text"
+          autocomplete="off"
           @focus="focusFunc()"
           @click="focusFunc()"
           @blur="blurFunc()"
